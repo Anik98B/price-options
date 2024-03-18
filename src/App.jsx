58693ffb914029cb550d57428234ba92adc63410
Anik98B <anikbhowmik0188@gmail.com>
2024-03-19 00:22:02 +0600

    create nav bar by self

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
     <>
 
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
       <h1 className='text-7xl bg-rose-500'>Vite + React</h1>
       
     </>
